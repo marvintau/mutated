bbfbb7961a2ccdfab153ea857d65e55f0e2efe0c
marvintau <nightvista@gmail.com>
2019-09-25 11:39:46 +0800

    added sum method to header

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ export default class Header extends List {
         super(...args);
     }
 
-    makeRecord(){
+    createRecord(){
 
         let defVals = {
             String: '',
@@ -28,5 +28,28 @@ export default class Header extends List {
         }
 
         return new Record(cols);
+    }
+
+    sum(list){
+
+        let newRec = this.createRecord();
+
+        for (let i = 0; i < this.length; i++){
+            let key = this[i].colKey,
+                type = this[i].type,
+                mapped = list.map(e => e.get(key));
+
+            if (type === 'String'){
+                let res = mapped.every((e, i, a) => e === a[0]) ? mapped[0] : '...';
+                newRec.set(key, res);
+            } else if (type === 'Number'){
+                let sum = mapped.reduce((acc, e) => acc + e, 0);
+                newRec.set(key, sum);
+            } else {
+                newRec.set(key, '...');
+            }
+        }
+
+        return newRec;
     }
 }
