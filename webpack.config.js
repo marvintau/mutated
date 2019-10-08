f07fe6f47ac9bfcdc807965e30a5875df70fe0f7
marvintau <nightvista@gmail.com>
2019-10-08 16:01:04 +0800

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-	entry: './src/Mutated.js',
+	entry: './src/Persisted.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'index.js',
-		library: 'mutated',
+		library: 'persisted',
 		libraryTarget: 'umd'
 	},
 	module: {
