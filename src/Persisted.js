62f3eb7f2dbef6f3753925dcca5d89c43710d20e
marvintau <nightvista@gmail.com>
2019-10-28 17:38:01 +0800

    added Sheet and Table structure

--- src/Persisted.js
+++ src/Persisted.js
@@ -2,3 +2,5 @@ export {default as Head} from './Head';
 export {default as Record} from './Record';
 export {default as Group} from './Group';
 export {default as List} from './List';
+export {default as Table} from './Table';
+export {default as Sheet} from './Sheet';
